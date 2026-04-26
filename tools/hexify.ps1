ac9077590fe15986a3c7f88e8762b112a1499b34
NcMasters <99719002+NcMasters@users.noreply.github.com>
2026-04-26 20:04:02 +0200

    Refactor paths to use project root directory

--- tools/hexify.ps1
+++ tools/hexify.ps1
@@ -1,5 +1,8 @@
-$htmlPath = '.\tef6686 web\radio.html'
-$dbPath = '.\tef6686 web\stations_db.js'
+# Get the main project folder (one level up from the 'tools' folder)
+$rootDir = Split-Path $PSScriptRoot -Parent
+
+$htmlPath = Join-Path $rootDir 'radio.html'
+$dbPath = Join-Path $rootDir 'stations_db.js'
 
 if (-not (Test-Path $htmlPath)) { Write-Error "Missing $htmlPath"; exit 1 }
 if (-not (Test-Path $dbPath)) { Write-Error "Missing $dbPath"; exit 1 }
@@ -26,12 +29,12 @@ function Get-HexGzip($str) {
 try {
     $hexHtml, $lenHtml = Get-HexGzip $html
     $outHtml = "const uint32_t WEB_HTML_LEN = $lenHtml;`nconst uint8_t WEB_HTML[] PROGMEM = {`n$hexHtml`n};"
-    $outHtmlFile = Join-Path $PSScriptRoot "html_hex.h"
+    $outHtmlFile = Join-Path $rootDir "html_hex.h"
     $outHtml | Out-File -FilePath $outHtmlFile -Encoding ASCII
 
     $hexDb, $lenDb = Get-HexGzip $db
     $outDb = "const uint32_t WEB_DB_LEN = $lenDb;`nconst uint8_t WEB_DB[] PROGMEM = {`n$hexDb`n};"
-    $outDbFile = Join-Path $PSScriptRoot "db_hex.h"
+    $outDbFile = Join-Path $rootDir "db_hex.h"
     $outDb | Out-File -FilePath $outDbFile -Encoding ASCII
     
     Write-Host "Successfully hexified web assets."
